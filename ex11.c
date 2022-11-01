1d85771645b7542958f616b73880eab397172924
CauãDias <94485488+UmCarinha222@users.noreply.github.com>
2022-11-01 19:49:43 -0300

    Add files via upload

---
+++ ex11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num,soma=0;
+
+    printf("Digite um numero:\n");
+   scanf("%d",&num);
+   if(num>0){
+       while(num>0){
+       soma=soma+num%10;
+       num=num/10;
+       }
+       printf("A soma dos algarismos eh:%d",soma);
+   }else{
+       printf("O numero eh invalido");
+   }
+   return 0;
+}
